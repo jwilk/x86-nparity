d512a47abc095aba5ba7965c7a0166989f9af376
Jakub Wilk <jwilk@jwilk.net>
2005-04-07 20:10:44 +0200

    Initial import.

---
+++ nparity.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+
+inline unsigned int nparity(unsigned int x)
+{
+  register unsigned int _v;
+  __asm__ ("or %1, %1\n\tmov $0, %0\n\tsetnp %b0" : "=r" (_v) : "r" (x));
+  return _v;
+}
+
+int main(void)
+{
+  unsigned int i;
+  for (i=0; i<64; i++)
+    printf("%cparity(%02d)\n", nparity(i)?'n':' ', i);
+}
+
+// vim:ts=2 sw=2 et
