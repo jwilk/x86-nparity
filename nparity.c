9a8034c59995d4e98d66be97bcb24b4aac2d4e34
Jakub Wilk <jwilk@jwilk.net>
2023-01-14 10:21:05 +0100

    Reindent.

--- nparity.c
+++ nparity.c
@@ -2,16 +2,22 @@
 
 static inline unsigned int nparity(unsigned int x)
 {
-  register unsigned int _v;
-  __asm__ ("or %1, %1\n\tmov $0, %0\n\tsetnp %b0" : "=r" (_v) : "r" (x));
-  return _v;
+    register unsigned int _v;
+    __asm__(
+        "or %1, %1\n"
+        "mov $0, %0\n"
+        "setnp %b0\n"
+        : "=r" (_v)
+        : "r" (x)
+    );
+    return _v;
 }
 
 int main(void)
 {
-  unsigned int i;
-  for (i=0; i<64; i++)
-    printf("%cparity(%02d)\n", nparity(i)?'n':' ', i);
+    unsigned int i;
+    for (i = 0; i < 64; i++)
+        printf("%cparity(%02d)\n", nparity(i) ? 'n' : ' ', i);
 }
 
-// vim:set ts=2 sts=2 sw=2 et:
+// vim:set ts=4 sts=4 sw=4 et:
